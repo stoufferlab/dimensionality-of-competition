23992770ba19ff1219be7049b52f9a49b1d871bc
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2016-09-26 13:20:46 +1300

    initial commit of code to analyze dimensionality of Godoy's hampa data

---
+++ code/Godoy/dev.fun.R
@@ -0,0 +1,30 @@
+
+dev.fun <- function(par, dimensions, x, y, weights, linkinv, dev.resids, targets, competitors, trace=FALSE){
+    # stuff from glm.fit that can probably be removed
+    # good <- weights > 0
+    # varmu <- variance(mu)[good]
+    # if (anyNA(varmu)) 
+    #     stop("NAs in V(mu)")
+    # if (any(varmu == 0)) 
+    #     stop("0s in V(mu)")
+    # mu.eta.val <- mu.eta(eta)
+    # if (any(is.na(mu.eta.val[good]))) 
+    #     stop("NAs in d(mu)/d(eta)")
+    # good <- (weights > 0) & (mu.eta.val != 0)
+    # if (all(!good)) {
+    #     conv <- FALSE
+    #     warning(gettextf("no observations informative at iteration %d", 
+    #       iter), domain = NA)
+    #     break
+    # }
+
+    coefs <- glm.coefs.from.traits(par, targets, competitors, dimensions, colnames(x))
+    
+    eta <- drop(x %*% coefs)
+    mu <- linkinv(eta)
+    dev <- sum(dev.resids(y, mu, weights))
+    if (trace) 
+        cat("Dimension = ", dimensions, " Deviance = ", dev,
+          "\n", sep = "")
+    return(dev)
+}
---
+++ code/Godoy/get.alphas.from.model.R
@@ -0,0 +1,14 @@
+
+# given a list of model coefficients, targets, and competitors, return an alpha matrix
+get.alphas.from.model <- function(start, targets, competitors){
+	alphas <- matrix(NA, length(targets), length(competitors)-1)
+	rownames(alphas) <- targets
+	colnames(alphas) <- competitors[which(competitors!="SOLO")]
+	for(j in rownames(alphas)){
+		for(k in colnames(alphas)){
+			alpha <- start[which(names(start) == paste0("target",j,":background",k,":neighbours_number"))]
+			alphas[j,k] <- alpha
+		}
+	}
+	return(alphas)
+}
