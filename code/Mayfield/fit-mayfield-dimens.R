084e509574b453ceb845bc41322b01d7bf203295
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2016-10-06 16:31:51 +1300

    updated version of mayfield analysis that uses new R package for code

--- code/Mayfield/fit-mayfield-dimens.R
+++ code/Mayfield/fit-mayfield-dimens.R
@@ -1,4 +1,7 @@
 
+# load in house competition library
+library(competition)
+
 # read in Margie's data
 datadir <- "../../data/Mayfield/"
 mayfield <- read.csv(paste0(datadir, "nhood data wTF.csv"))
@@ -6,6 +9,11 @@ mayfield <- read.csv(paste0(datadir, "nhood data wTF.csv"))
 # remove NA and 0 
 # TO-DO: ask Margie about zero fecundities
 mayfield <- subset(mayfield, !is.na(seeds) & !(seeds==0))
+
+mayfield[which(mayfield$light=="Open"),"light"] <- "open"
+mayfield[which(mayfield$light=="Shade"),"light"] <- "shade"
+
+mayfield$light <- droplevels(mayfield$light)
 
 # # turn the NAs for background into a non-existent species SOLO which will help us when using the glm function (or equivalents) for fitting
 # levels(mayfield$background) <- c(levels(mayfield$background),"SOLO")
@@ -27,27 +35,41 @@ mayfield <- mayfield[,c('seeds','focal',competitors,'datasource','plotID','block
 ### Attempt things just by optimizing the traits
 ###############################################################################
 ###############################################################################
-source('dev.fun.R')
-source('get.alphas.from.model.R')
-source('glm.coefs.from.traits.R')
-source('change.dimensions.R')
-source('optimal.traits.R')
 
 optim.lowD <- list()
 for(t in levels(mayfield$light)){
 	optim.lowD[[t]] <- list()
 
 	# let's try just one treatment for simplicity in testing regime
-	mayfield.local <- subset(mayfield, (light=="Shade" | light == "shade"))
+	mayfield.local <- subset(mayfield, light==t)
 	focals.local <- levels(droplevels(mayfield.local$focal))
 	competitors.local <- competitors[which(colSums(mayfield.local[,competitors])>0)]
 
-	dimensions <- 1
-	optim.lowD[[t]][[dimensions]] <- optimal.traits(mayfield.local, dimensions, focals.local, competitors.local, ftol_rel=1E-8, maxeval=5E6)
+	# solve the zero- and one-dimensional problems first
+	dimensions <- 0
+	tmp <- optimal.traits(mayfield.local, dimensions, focals.local, competitors.local, method='sbplx', maxeval=1E6)
+	# tmp <- optimal.traits(mayfield.local, dimensions, focals.local, competitors.local, start=tmp$par, method='cobyla', maxeval=10000)
+	optim.lowD[[t]][[as.character(dimensions)]] <- tmp
 
-	for(dimensions in seq.int(2,10)){
-		start <- change.dimensions(optim.lowD[[t]][[dimensions-1]]$par, focals, competitors, dimensions-1, dimensions)
-		optim.lowD[[t]][[dimensions]] <- optimal.traits(mayfield.local, dimensions, focals.local, competitors.local, start=start, ftol_rel=1E-8, maxeval=5E6)
+	dimensions <- 1
+	while(TRUE){
+		tmp <- optimal.traits(mayfield.local, dimensions, focals.local, competitors.local, method='sbplx', maxeval=1E6)
+		if(is.finite(tmp$value)) break
+	}
+	# tmp <- optimal.traits(mayfield.local, dimensions, focals.local, competitors.local, start=tmp$par, method='cobyla', maxeval=10000)
+	optim.lowD[[t]][[as.character(dimensions)]] <- tmp
+
+	# now use the lower-dimensional solutions as starting points for the more complex ones
+	for(dimensions in seq.int(2,min(length(focals.local),length(competitors.local)))){
+		start <- change.dimensions(optim.lowD[[t]][[as.character(dimensions-1)]]$par, focals.local, competitors.local, dimensions-1, dimensions)
+
+		while(TRUE){
+			tmp <- optimal.traits(mayfield.local, dimensions, focals.local, competitors.local, start=start, method='sbplx', maxeval=1E6)
+			if(is.finite(tmp$value)) break
+		}
+
+		# tmp <- optimal.traits(mayfield.local, dimensions, focals.local, competitors.local, start=tmp$par, method='cobyla', maxeval=10000)
+		optim.lowD[[t]][[as.character(dimensions)]] <- tmp
 	}
 
 }
