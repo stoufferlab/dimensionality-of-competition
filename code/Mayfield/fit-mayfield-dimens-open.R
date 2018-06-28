6675a062c365ea2e5fbcbc43d7a6c1f0eafe3b67
Daniel B. Stouffer <dbstouffer@gmail.com>
2018-06-28 15:07:56 -0600

    latest incarnation of the Mayfield analysis

---
+++ code/Mayfield/fit-mayfield-dimens-open.R
@@ -0,0 +1,29 @@
+
+# read in Margie's data
+datadir <- "../../data/Mayfield/"
+mayfield <- read.csv(paste0(datadir, "nhood data wTF.csv"))
+
+cc <- colnames(mayfield)
+cc[5] <- "target"
+colnames(mayfield) <- cc
+
+competitors <- colnames(mayfield)[9:39]
+
+# remove NA and 0 
+# WARNING: ask Margie about zero fecundities
+mayfield <- subset(mayfield, !is.na(seeds) & !(seeds==0))
+
+# let's try just one treatment for simplicity in testing regime
+mayfield <- subset(mayfield, light=="Open")
+
+# the data should now be primed for analysis; woohoo!
+
+# run the fitting code
+source('fit.machine.R')
+
+# save the fits and write out a table of the AICs
+Open.optim.lowD <- optim.lowD
+save(Open.optim.lowD, file="Open.optim.lowD.Rdata")
+
+Open.AICs <- cbind(0:length(optim.lowD), c(summary(null.fit)$aic, unlist(lapply(optim.lowD, function(x){x$aic}))))
+write.table(Open.AICs, "../../data/Mayfield/mayfield.Open.AICs.csv", quote=FALSE, col.names=FALSE, sep=" ", row.names=FALSE)
---
+++ code/Mayfield/fit-mayfield-dimens-shade.R
@@ -0,0 +1,29 @@
+
+# read in Margie's data
+datadir <- "../../data/Mayfield/"
+mayfield <- read.csv(paste0(datadir, "nhood data wTF.csv"))
+
+cc <- colnames(mayfield)
+cc[5] <- "target"
+colnames(mayfield) <- cc
+
+competitors <- colnames(mayfield)[9:39]
+
+# remove NA and 0 
+# WARNING: ask Margie about zero fecundities
+mayfield <- subset(mayfield, !is.na(seeds) & !(seeds==0))
+
+# let's try just one treatment for simplicity in testing regime
+mayfield <- subset(mayfield, light=="Shade")
+
+# the data should now be primed for analysis; woohoo!
+
+# run the fitting code
+source('fit.machine.R')
+
+# save the fits and write out a table of the AICs
+Shade.optim.lowD <- optim.lowD
+save(Shade.optim.lowD, file="Shade.optim.lowD.Rdata")
+
+Shade.AICs <- cbind(0:length(optim.lowD), c(summary(null.fit)$aic, unlist(lapply(optim.lowD, function(x){x$aic}))))
+write.table(Shade.AICs, "../../data/Mayfield/mayfield.Shade.AICs.csv", quote=FALSE, col.names=FALSE, sep=" ", row.names=FALSE)
