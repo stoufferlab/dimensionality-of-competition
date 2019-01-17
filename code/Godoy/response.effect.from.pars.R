4dcec5bf2398710051966951cc0e89fba73cccc6
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2019-01-17 16:37:15 +1300

    code to extract the intelligible traits and coefficients from the lowest AIC fits

---
+++ code/Godoy/response.effect.from.pars.R
@@ -0,0 +1,29 @@
+
+response.effect.from.pars <- function(par, targets, competitors, dimensions){
+    lambda <- 1. / par[seq.int(length(targets))]
+
+    # turn the linear version of the response traits into a species by trait matrix
+    response.traits <- matrix(
+        par[seq.int(length(targets)+1, length(targets)+length(targets)*dimensions)],
+        length(targets),
+        dimensions
+    )
+
+    # since we use the Gamma form we need to pull out the effect of lambda
+    response.traits <- sweep(response.traits, 1, par[seq.int(length(targets))], "/")
+
+    # names help us later
+    rownames(response.traits) <- targets
+
+    # turn the linear version of the effect traits into a species by trait matrix
+    effect.traits <- matrix(
+        par[seq.int(length(targets)+length(targets)*dimensions+1, length(targets)+length(targets)*dimensions+(length(competitors)-1)*dimensions)],
+        length(competitors)-1,
+        dimensions
+    )
+
+    # names help us later
+    rownames(effect.traits) <- competitors[competitors!="SOLO"]
+
+    return(list(lambda=lambda, response=response.traits, effect=effect.traits))
+}
