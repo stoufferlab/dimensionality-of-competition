a87b22465e875b2236270ee8df3ddc70519c36e6
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2019-01-23 16:27:04 +1300

    restructuring to use with orthogonality approach

--- code/Mayfield/response.effect.from.pars.R
+++ code/Mayfield/response.effect.from.pars.R
@@ -1,30 +1,63 @@
 
 response.effect.from.pars <- function(par, targets, competitors, dimensions){
-    lambda <- 1. / par[seq.int(length(targets))]
-    names(lambda) <- targets
+    # the intrinsic fecundities come first
+    lambdas <- 1. / par[seq.int(length(targets))]
+    names(lambdas) <- targets
 
-    # turn the linear version of the response traits into a species by trait matrix
-    response.traits <- matrix(
-        par[seq.int(length(targets)+1, length(targets)+length(targets)*dimensions)],
-        length(targets),
-        dimensions
-    )
+    # "eigenvalues" for the different dimensions come next
+    weights <- exp(par[seq.int(length(lambdas)+1, length(lambdas)+dimensions)])
 
-    # since we use the Gamma form we need to pull out the effect of lambda
-    response.traits <- sweep(response.traits, 1, lambda, "/")
+    # angles for response traits come next
+    n.angles <- (choose(length(targets),2) - choose(length(targets)-dimensions,2))
+    response.angles <- par[seq.int(
+        from=length(targets)+length(weights)+1,
+        to=length(targets)+length(weights)+n.angles
+    )]
 
-    # names help us later
+    # angles for effect traits come last
+    n.angles <- (choose(length(competitors),2) - choose(length(competitors)-dimensions,2))
+    effect.angles <- par[seq.int(
+        from=length(targets)+length(weights)+length(response.angles)+1,
+        to=length(targets)+length(weights)+length(response.angles)+n.angles
+    )]
+
+    # now we need to convert the response and effect angles into usable low-dimensional traits
+    # we first need to tack on zeros at the end
+    response.angles <- c(response.angles, rep(0,choose(length(targets),2) - length(response.angles)))
+
+    # now we turn angles into 
+    response.traits <- gea_orthogonal_from_angles(response.angles)
+
+    # and we use the last columns as the actual response traits
+    response.traits <- response.traits[,seq.int(ncol(response.traits),ncol(response.traits)-dimensions+1),drop=FALSE]
+
+    # name things
     rownames(response.traits) <- targets
+    colnames(response.traits) <- paste0("response",seq.int(dimensions))
 
-    # turn the linear version of the effect traits into a species by trait matrix
-    effect.traits <- matrix(
-        par[seq.int(length(targets)+length(targets)*dimensions+1, length(targets)+length(targets)*dimensions+(length(competitors))*dimensions)],
-        length(competitors),
-        dimensions
-    )
+    # and again for the effects
+    # we first need to tack on zeros at the end
+    effect.angles <- c(effect.angles, rep(0,choose(length(competitors),2) - length(effect.angles)))
 
-    # names help us later
+    # now we turn angles into 
+    effect.traits <- gea_orthogonal_from_angles(effect.angles)
+
+    # and we use the last columns as the actual response traits
+    effect.traits <- effect.traits[,seq.int(ncol(effect.traits),ncol(effect.traits)-dimensions+1),drop=FALSE]
+
+    # name things
     rownames(effect.traits) <- competitors
+    colnames(effect.traits) <- paste0("effect",seq.int(dimensions))
 
-    return(list(lambda=lambda, response=response.traits, effect=effect.traits))
+    # generate alphas from matrix multiplication!
+    alphas <- response.traits %*% diag(weights,dimensions,dimensions) %*% t(effect.traits)
+    rownames(alphas) <- targets
+    colnames(alphas) <- competitors
+
+    # lets reorder things in decreasing order of the weights across each dimension
+    response.traits <- response.traits[,order(weights,decreasing = TRUE),drop=FALSE]
+    effect.traits <- effect.traits[,order(weights,decreasing = TRUE),drop=FALSE]
+    weights <- weights[order(weights, decreasing = TRUE)]
+
+    return(list(lambdas=lambdas, weights=weights, response=response.traits, effect=effect.traits, alphas=alphas))
 }
