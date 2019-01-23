4ed7d69ff08c294ef256040c008ce12970b67816
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2019-01-23 16:28:25 +1300

    removing redundancies and using the Mayfield code since they are part and parcel

--- code/Goldberg/fit-goldberg-dimens.R
+++ code/Goldberg/fit-goldberg-dimens.R
@@ -15,6 +15,7 @@ for(focal in 1:7){
 goldberg <- goldberg[,c("biomass.inv","focal",paste0("sp.",1:7))]
 goldberg$biomass <- 1 / goldberg$biomass.inv
 
+# focal column is a factor
 goldberg$focal <- as.factor(goldberg$focal)
 
 # par(mfrow=c(7,7))
@@ -48,16 +49,32 @@ cc <- colnames(goldberg2)
 cc[2] <- "target"
 colnames(goldberg2) <- cc
 
+# rename the core data frame
+fecundity.data <- goldberg2
+
+# we need a variable called targets for things to work
+targets <- levels(fecundity.data$target)
+
+# we need a variable called competitors for things to work
 competitors <- paste0("sp.",1:7)
+
+# we need to know what column has the fecundities
+fecundity <- "biomass"
+
+# specify the model family to fit
+which.family <- Gamma()
+
+# number of random starts for optimization at each dimension
+n.random <- 25
 
 # the data should now be primed for analysis; woohoo!
 
 # run the fitting code
-source('fit.machine.R')
+source('../Mayfield/fit.machine.R')
 
 # save the fits
 Goldberg.optim.lowD <- optim.lowD
-save(Goldberg.optim.lowD, file="../../results/Goldberg/Goldberg.optim.lowD.Rdata")
+save(Goldberg.optim.lowD, file="../../results/Goldberg/Goldberg.optim.lowD.Rdata", ascii = TRUE)
 
 # write out a table of the AICs
 Goldberg.AICs <- cbind(0:length(optim.lowD), c(summary(null.fit)$aic, unlist(lapply(optim.lowD, function(x){x$aic}))))
