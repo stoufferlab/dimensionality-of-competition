c9c3ca89afbb6af869f40314641d3c4f89161d99
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2019-01-25 15:30:21 +1300

    we could now use abacus if needed (and it's needed)

--- code/Mayfield/fit-mayfield-dimens-open.R
+++ code/Mayfield/fit-mayfield-dimens-open.R
@@ -40,15 +40,23 @@ which.family <- Gamma()
 # rename the core data frame
 fecundity.data <- mayfield
 
-# number of random starts for optimization at each dimension
-n.random <- 25
+# we get some input from the command line
+args <- commandArgs(trailingOnly = TRUE)
+
+# the core dimension for this optimization
+which.dimension <- as.integer(args[1])
+
+# which of n random optimizations this is
+which.n.random <- as.integer(args[2])
 
 # run the dimensionality fitting code
 source('fit.machine.R')
 
 # save the fits and write out a table of the AICs
 Open.optim.lowD <- optim.lowD
-save(Open.optim.lowD, file="../../results/Mayfield/Open.optim.lowD.Rdata", ascii=TRUE)
-
-Open.AICs <- cbind(0:length(optim.lowD), c(summary(null.fit)$aic, unlist(lapply(optim.lowD, function(x){x$aic}))))
-write.table(Open.AICs, "../../results/Mayfield/mayfield.Open.AICs.csv", quote=FALSE, col.names=FALSE, sep=" ", row.names=FALSE)
+save(Open.optim.lowD,
+	file=paste0("../../results/Mayfield/Open.optim.D",which.dimension,".r",which.n.random,".Rdata"),
+	ascii = TRUE
+)
+# Open.AICs <- cbind(0:length(optim.lowD), c(summary(null.fit)$aic, unlist(lapply(optim.lowD, function(x){x$aic}))))
+# write.table(Open.AICs, "../../results/Mayfield/mayfield.Open.AICs.csv", quote=FALSE, col.names=FALSE, sep=" ", row.names=FALSE)
--- code/Mayfield/fit-mayfield-dimens-shade.R
+++ code/Mayfield/fit-mayfield-dimens-shade.R
@@ -38,15 +38,25 @@ which.family <- Gamma()
 # rename the core data frame
 fecundity.data <- mayfield
 
-# number of random starts for optimization at each dimension
-n.random <- 25
+# we get some input from the command line
+args <- commandArgs(trailingOnly = TRUE)
+
+# the core dimension for this optimization
+which.dimension <- as.integer(args[1])
+
+# which of n random optimizations this is
+which.n.random <- as.integer(args[2])
 
 # run the dimensionality fitting code
 source('fit.machine.R')
 
 # save the fits and write out a table of the AICs
 Shade.optim.lowD <- optim.lowD
-save(Shade.optim.lowD, file="../../results/Mayfield/Shade.optim.lowD.Rdata", ascii=TRUE)
+# save(Shade.optim.lowD, file="../../results/Mayfield/Shade.optim.lowD.Rdata", ascii=TRUE)
+save(Shade.optim.lowD,
+	file=paste0("../../results/Mayfield/Shade.optim.D",which.dimension,".r",which.n.random,".Rdata"),
+	ascii = TRUE
+)
 
-Shade.AICs <- cbind(0:length(optim.lowD), c(summary(null.fit)$aic, unlist(lapply(optim.lowD, function(x){x$aic}))))
-write.table(Shade.AICs, "../../results/Mayfield/mayfield.Shade.AICs.csv", quote=FALSE, col.names=FALSE, sep=" ", row.names=FALSE)
+# Shade.AICs <- cbind(0:length(optim.lowD), c(summary(null.fit)$aic, unlist(lapply(optim.lowD, function(x){x$aic}))))
+# write.table(Shade.AICs, "../../results/Mayfield/mayfield.Shade.AICs.csv", quote=FALSE, col.names=FALSE, sep=" ", row.names=FALSE)
