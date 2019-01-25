481f8408da338f29307c07fce1d3fb6267da3073
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2019-01-25 15:41:40 +1300

    godoy to fit with abacus too

--- code/Godoy/fit-hampa-dimens-C.R
+++ code/Godoy/fit-hampa-dimens-C.R
@@ -30,15 +30,24 @@ fecundity <- "fruits"
 # specify the model family to fit
 which.family <- Gamma()
 
-# number of random starts for optimization at each dimension
-n.random <- 25
+# we get some input from the command line
+args <- commandArgs(trailingOnly = TRUE)
+
+# the core dimension for this optimization
+which.dimension <- as.integer(args[1])
+
+# which of n random optimizations this is
+which.n.random <- as.integer(args[2])
 
 # run the fitting code
 source('fit.machine.R')
 
 # save the fits and write out a table of the AICs
 C.optim.lowD <- optim.lowD
-save(C.optim.lowD, file="../../results/Godoy/C.optim.lowD.Rdata", ascii=TRUE)
+save(C.optim.lowD,
+	file=paste0("../../results/Godoy/C.optim.D",which.dimension,".r",which.n.random,".Rdata"),
+	ascii = TRUE
+)
 
-C.AICs <- cbind(0:length(optim.lowD), c(summary(null.fit)$aic, unlist(lapply(optim.lowD, function(x){x$aic}))))
-write.table(C.AICs, "../../results/Godoy/godoy.C.AICs.csv", quote=FALSE, col.names=FALSE, sep=" ", row.names=FALSE)
+# C.AICs <- cbind(0:length(optim.lowD), c(summary(null.fit)$aic, unlist(lapply(optim.lowD, function(x){x$aic}))))
+# write.table(C.AICs, "../../results/Godoy/godoy.C.AICs.csv", quote=FALSE, col.names=FALSE, sep=" ", row.names=FALSE)
