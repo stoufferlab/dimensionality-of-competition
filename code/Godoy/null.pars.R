0cee20a30c213074b64ce0cadecf60240bbe5db3
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2019-01-29 16:48:25 +1300

    improved starting parameters

--- code/Godoy/null.pars.R
+++ code/Godoy/null.pars.R
@@ -1,11 +1,15 @@
 
 # given the data set up some null parameters to start with in the optimizer
-null.pars <- function(null.fit, targets, competitors, dimensions, random.angles=FALSE){
-	# lambdas and weightings are more straightforward
-	lambdas <- coef(null.fit) # lambdas
+null.pars <- function(targets, competitors, dimensions, random.angles=FALSE, godoy=FALSE, lambdas=NULL){
+	# lambdas and weights are more straightforward
+	if(is.null(lambdas)){
+		lambdas <- log(rep(1,length(targets)))
+	}else{
+		lambdas <- log(lambdas)
+	}
 	names(lambdas) <- targets
 
-	weights <- log(sort(runif(dimensions))) # weights
+	weights <- log(0.01 / seq.int(dimensions)) # weights; note log transformation
 	names(weights) <- paste0("weight",seq.int(dimensions))
 	
 	# the number of response and effect angles is trickier
@@ -26,12 +30,12 @@ null.pars <- function(null.fit, targets, competitors, dimensions, random.angles=
 	}
 	names(response.angles) <- paste0("response",seq.int(length(response.angles)))
 
-	effect.dof <- seq.int(length(competitors)-2,0)
+	effect.dof <- seq.int(length(competitors)-1-godoy,0)
 	if(!random.angles){
 		effect.angles <- rep(0, sum(effect.dof[seq.int(dimensions)]))	
 	}else{
 		effect.angles <- unlist(sapply(
-			effect.dof[seq.int(min(dimensions,length(competitors)-2))],
+			effect.dof[seq.int(min(dimensions,length(competitors)-1-godoy))],
 			function(x){
 				# the first n-1 are in [-pi/2, pi/2]
 				# the last one is in [-pi, pi]
