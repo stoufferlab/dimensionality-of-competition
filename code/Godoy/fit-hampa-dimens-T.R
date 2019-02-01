98018cbe84a44118a288e3c9e4942e6a5a4f3927
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2019-02-01 17:12:37 +1300

    updated Godoy analysis

--- code/Godoy/fit-hampa-dimens-T.R
+++ code/Godoy/fit-hampa-dimens-T.R
@@ -1,33 +1,7 @@
 
-# read in Oscar's data
-datadir <- "../../data/Godoy/"
-
-# read in the raw data
-hampa <- read.csv(paste0(datadir, "hampa_neigbours_survey.csv"), row.names=1)
-
-# remove the extra columns at the end
-hampa <- hampa[,which(!grepl("X",colnames(hampa)))]
-
-# turn the NAs for background into a non-existent species SOLO which will help us when using the glm function (or equivalents) for fitting
-levels(hampa$background) <- c(levels(hampa$background),"SOLO")
-hampa$background[which(is.na(hampa$background))] <- "SOLO"
-
-# the data should now be primed for analysis; woohoo!
-
-# let's try just one treatment for simplicity in testing regime
-hampa <- subset(hampa, treatment=="T")
-
-# rename the core data frame
-fecundity.data <- hampa
-
-# we need a variable called targets for things to work
-targets <- levels(fecundity.data$target)
-
-# we need a variable called competitors for things to work
-competitors <- levels(fecundity.data$background)
-
-# we need to know what column has the fecundities
-fecundity <- "fruits"
+# read in the data for the specified treatment
+which.treament <- "T"
+source('prep.data.R')
 
 # specify the model family to fit
 which.family <- Gamma()
@@ -40,6 +14,9 @@ which.dimension <- as.integer(args[1])
 
 # which of n random optimizations this is
 which.n.random <- as.integer(args[2])
+
+# # run the model comparison code
+# source('model.comparison.R')
 
 # run the fitting code
 source('fit.machine.R')
