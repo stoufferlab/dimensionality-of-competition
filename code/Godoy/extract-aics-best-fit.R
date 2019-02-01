a928277294c581a096a1b70bb7321d1e4072b75a
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2019-02-01 17:13:10 +1300

    forgot this one

---
+++ code/Godoy/extract-aics-best-fit.R
@@ -0,0 +1,70 @@
+
+# a few utilities we need below
+source('./response.effect.from.pars.R')
+source('./polar.transform.R')
+
+for(which.treatment in c('C','T')){
+
+	# read in the mayfield data for the specified treatment and assign some common variables
+	source('prep.data.R')
+
+	# scrape the shit out of the output files
+	best.fits <- lapply(
+		seq.int(length(targets)),
+		function(d){
+			saved.fits <- list.files('../../results/Godoy/', pattern=paste0(which.treatment,'.optim.D',d))
+			aics <- unlist(sapply(
+				saved.fits,
+				function(x){
+					q <- try(load(paste0('../../results/Godoy/',x)))
+					if(inherits(q, "try-error")){
+						message(x)
+						return(NA)
+					}else{
+						assign("y", eval(parse(text = paste0(which.treatment,".optim.lowD"))))
+						aa <- y[[1]]$aic
+						ifelse(is.null(aa),NA,aa)
+					}
+				}
+			))
+		}
+	)
+
+	# troll through the lists and find the lowest at each dimension and the best overall
+	best.aics <- unlist(lapply(best.fits, min, na.rm=TRUE))
+	best.val <- min(best.aics)
+	delta.aics <- best.aics - best.val
+	best.d <- min(which(delta.aics <= 2))
+	bestest <- which.min(best.fits[[best.d]])
+	bestest <- names(best.fits[[best.d]][bestest])
+
+	# reload the best overall model and save the interpretable parameters to a separate unique file
+	load(paste0('../../results/Godoy/', bestest))
+	assign("y", eval(parse(text = paste0(which.treatment,".optim.lowD"))))
+	Godoy.best <- response.effect.from.pars(
+		y[[1]]$par,
+		targets,
+		competitors,
+		dimensions=best.d,
+		godoy=TRUE
+	)
+	save(Godoy.best,
+		file=paste0("../../results/Godoy/godoy.",which.treatment,".best.Rdata"),
+		ascii = TRUE
+	)
+
+	# fit the classic models as a point of comparison for the AIC figures
+	source('./model.comparison.R')
+
+	# write out a table of the AICs
+	Godoy.AICs <- c(gamma.fit.0$aic, best.aics)
+	Godoy.AICs <- cbind(seq.int(length(Godoy.AICs))-1, Godoy.AICs)
+	write.table(
+		Godoy.AICs,
+		paste0("../../results/Godoy/godoy.",which.treatment,".AICs.csv"),
+		quote=FALSE,
+		col.names=FALSE,
+		sep=" ",
+		row.names=FALSE
+	)
+}
