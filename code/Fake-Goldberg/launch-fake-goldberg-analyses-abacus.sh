270fff56d2eab8f4134f31acd6bb46e04e22c499
Daniel B. Stouffer <stouffer@abacus>
2019-02-21 11:27:44 +1300

    fewer random starts for the fake data to keep the number of files manageable

--- code/Fake-Goldberg/launch-fake-goldberg-analyses-abacus.sh
+++ code/Fake-Goldberg/launch-fake-goldberg-analyses-abacus.sh
@@ -5,7 +5,7 @@ let NRAND=25
 let MAX=7
 for((r=MAX;r>=1;--r)); do
 	for((d=MAX;d>=1;--d)); do
-  		qsub -t 1:250 ./qsub-fake-goldberg.sh $r $d;
+  		qsub -t 1:50 ./qsub-fake-goldberg.sh $r $d;
 	done
 done
 
