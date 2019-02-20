7935d9eb0a9cb86e0b9924d091f842d277cf5f41
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2019-02-21 10:24:29 +1300

    fit the low d approximation for a specified fake data set

---
+++ code/Fake-Goldberg/fit-fake-goldberg-dimens.R
@@ -0,0 +1,28 @@
+
+# we get some input from the command line
+args <- commandArgs(trailingOnly = TRUE)
+
+# the core dimension for this optimization
+which.fake.data <- as.integer(args[1])
+
+# read in the fake goldberg data
+source('prep.data.R')
+
+# specify the model family to fit
+which.family <- Gamma()
+
+# the core dimension for this optimization
+which.dimension <- as.integer(args[2])
+
+# which of n random optimizations this is
+which.n.random <- as.integer(args[3])
+
+# run the fitting code
+source('../Mayfield/fit.machine.R')
+
+# save the fits
+Goldberg.optim.lowD <- optim.lowD
+save(Goldberg.optim.lowD,
+	file=paste0("../../results/Fake-Goldberg/Fake-Goldberg-",which.fake.data,".optim.D",which.dimension,".r",which.n.random,".Rdata"),
+	ascii = TRUE
+)
