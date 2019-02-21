a6e6f42563e58a803587bdf0c3fcfa6df632eebd
Daniel B. Stouffer <stouffer@abacus>
2019-02-22 10:27:42 +1300

    the whole shabang

--- code/Fake-Goldberg/launch-fake-goldberg-analyses-abacus.sh
+++ code/Fake-Goldberg/launch-fake-goldberg-analyses-abacus.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 typeset -i r NRAND d MAX
+
+# there are 25 random data sets and 7 possible dimensionalities
 let NRAND=25
 let MAX=7
-for((r=MAX;r>=1;--r)); do
-	for((d=MAX;d>=1;--d)); do
+
+# go through all random data sets r for all dimensions d
+for((r=NRAND;r>=1;--r)); do
+  for((d=MAX;d>=1;--d)); do
   		qsub -t 1:250 ./qsub-fake-goldberg.sh $r $d;
 	done
 done
