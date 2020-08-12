7c3e2f0b7f7ea5f56878aaea03af2a279942c0ac
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2020-08-12 13:56:07 +1200

    a few notes for my own sanity

--- code/Godoy/glm.coefs.from.traits.R
+++ code/Godoy/glm.coefs.from.traits.R
@@ -8,9 +8,11 @@ glm.coefs.from.traits <- function(par, targets, competitors, dimensions, xnames)
     names(coefs) <- xnames
     
     # add the lambdas to the appropriate places
+    # DEBUG: note transformation because of Gamma family
     coefs[paste0("target",targets)] <- 1. / par$lambdas
 
     # add the alphas to the appropriate places
+    # DEBUG: note transformation because of Gamma family
     for(i in rownames(par$alphas)){
         for(j in colnames(par$alphas)){
             coefs[paste0("target",i,":background",j,":neighbours_number")] <- (1. / par$lambdas[i]) * par$alphas[i,j]
