4e4711702a831abed96bf6fe3e6edce934cde1fd
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2020-10-13 15:03:35 +1300

    quick script to generate all plots

---
+++ code/Figures/runme.R
@@ -0,0 +1,21 @@
+
+# figure 1
+source('alpha_matrices_godoy_T.R')
+# figure 2
+source('dimensionality.R')
+# figure 3
+source('response_effect_godoy_T.R')
+# figure 4
+source('control_treatment_godoy.R')
+
+# supps
+# response effect traits for all other datasets
+source('response_effect_others.R')
+# observed vs fit matrices for all datasets
+source('alpha_matrices_others.R')
+# R^2 and variance per dimension for all datasets
+source('rsquared.R')
+source('variance_explained.R')
+
+# testing
+source('control_treatment_wainwright.R')
