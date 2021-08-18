2deaea75c5017bbf6c46f04a5ebc7f8e46b512e1
Daniel B. Stouffer <stouffer@abacus>
2021-08-19 10:24:36 +1200

    fewer replicate fits

--- code/Spain/launch-spain-analyses-abacus.sh
+++ code/Spain/launch-spain-analyses-abacus.sh
@@ -3,6 +3,6 @@
 typeset -i d MAX
 let MAX=10
 for((d=10;d>=1;--d)); do
-  qsub -t 1:250 ./qsub-spain-control.sh $d;
-  qsub -t 1:250 ./qsub-spain-treatment.sh $d;
+  qsub -t 1:50 ./qsub-spain-control.sh $d;
+  qsub -t 1:50 ./qsub-spain-treatment.sh $d;
 done
