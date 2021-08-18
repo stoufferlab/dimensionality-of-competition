c7a6ea2068ec73fc9b4903361fb7179ad7770331
Daniel B. Stouffer <stouffer@abacus>
2021-08-19 10:25:28 +1200

    fewer replicate fits

--- code/Australia/launch-australia-analyses-abacus.sh
+++ code/Australia/launch-australia-analyses-abacus.sh
@@ -3,7 +3,7 @@
 typeset -i d MAX
 let MAX=8
 for((d=MAX;d>=1;--d)); do
-  qsub -t 1:250 ./qsub-australia-open.sh $d;
-  qsub -t 1:250 ./qsub-australia-shade.sh $d;
+  qsub -t 1:50 ./qsub-australia-open.sh $d;
+  qsub -t 1:50 ./qsub-australia-shade.sh $d;
 done
 
