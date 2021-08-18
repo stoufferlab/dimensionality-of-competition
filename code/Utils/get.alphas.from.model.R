ec6302c0b5fa2e19b8b4125f03062024f9ff1fcd
Daniel B. Stouffer <dbstouffer@gmail.com>
2021-08-19 09:29:18 +1200

    allow the user to "zero out" non significant interaction coefficients

--- code/Utils/get.alphas.from.model.R
+++ code/Utils/get.alphas.from.model.R
@@ -1,35 +1,47 @@
 
 # given a regression model, its "type", targets, and competitors,
 # return its implied lambdas and alpha matrix
-get.alphas.from.model <- function(model, model.type=c(0,1,2,3,4), targets, competitors){
+get.alphas.from.model <- function(model, model.type=c(0,1,2,3,4), targets, competitors, zeros=FALSE){
 	# TO DO: determine model type directly from model coefficients
-	coefs <- coef(model)
+	coefs <- coef(summary(model))
 
 	# the intercepts are the easy part
-	lambdas <- 1 / coefs[paste0("target",targets)]
+	lambdas <- 1 / coefs[paste0("target",targets), "Estimate"]
 
 	# generate a container for alphas coefficients
-	alphas <- matrix(NA, length(targets), length(competitors))
+	alphas <- matrix(0, length(targets), length(competitors))
 	rownames(alphas) <- targets
 	colnames(alphas) <- competitors
 
 	# populate the alpha coefficients as a function of the model construct
 	if(model.type == 0){
-		alphas[targets, competitors] <- 0
+		# alphas[targets, competitors] <- 0
 	}else if(model.type == 1){
-		alphas[targets, competitors] <- coefs["neighbors"]
+		if(!zeros || coefs["neighbors", "Pr(>|z|)"] < 0.05){
+			alphas[targets, competitors] <- coefs["neighbors", "Estimate"]
+		}
 	}else if(model.type == 2){
 		for(i in targets){
-			alphas[i,] <- coefs[paste0("target",i,":neighbors")]
+			if(!zeros || coefs[paste0("target",i,":neighbors"), "Pr(>|z|)"] < 0.05){
+				alphas[i,] <- coefs[paste0("target",i,":neighbors"), "Estimate"]
+			}
 		}
 	}else if(model.type == 3){
 		for(j in competitors){
-			alphas[,j] <- coefs[j]
+			if(j %in% rownames(coefs)){
+				if(!zeros || coefs[j, "Pr(>|z|)"] < 0.05){
+					alphas[,j] <- coefs[j, "Estimate"]
+				}
+			}
 		}
 	}else if(model.type == 4){
 		for(i in targets){
 			for(j in competitors){
-				alphas[i,j] <- coefs[paste0("target",i,":",j)]
+				if(paste0("target",i,":",j) %in% rownames(coefs)){
+					if(!zeros || coefs[paste0("target",i,":",j), "Pr(>|z|)"] < 0.05){
+						alphas[i,j] <- coefs[paste0("target",i,":",j), "Estimate"]
+					}
+				}
 			}
 		}
 	}else{
