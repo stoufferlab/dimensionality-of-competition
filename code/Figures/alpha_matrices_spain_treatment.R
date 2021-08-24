2b56e02751d505d51761774a7762e2f87b66d4b3
Daniel B. Stouffer <dbstouffer@gmail.com>
2021-08-25 10:14:34 +1200

    renamed and tweaked figure for decomposed spanish alpha matrix

--- code/Figures/alpha_matrices_spain_treatment.R
+++ code/Figures/alpha_matrices_spain_treatment.R
@@ -1,30 +1,34 @@
 
 # library(ggplot2)
-library(RColorBrewer)
+library(here)
 library(plot.matrix)
+library(RColorBrewer)
 
-# f = a + b1*z + b2*N + b3*E + c1*z*z + c2*N*N + c3*E*E + c4*z*N + c5*z*E + c6*N*E + d*z*N*E
+# determine the best 
 
-A1 <- as.matrix(read.table("../../results/Godoy/godoy.T.alphas.1.csv"))
-A2 <- as.matrix(read.table("../../results/Godoy/godoy.T.alphas.2.csv"))
-A3 <- as.matrix(read.table("../../results/Godoy/godoy.T.alphas.3.csv"))
-A <- A1 + A2 + A3 #as.matrix(read.table("../../results/Godoy/godoy.T.alphas.csv"))
+A <- as.matrix(read.table("../../results/Spain/spain.Treatment.full.alphas.fit.csv"))
+A1 <- as.matrix(read.table("../../results/Spain/spain.Treatment.full.alphas.1.fit.csv"))
+A2 <- as.matrix(read.table("../../results/Spain/spain.Treatment.full.alphas.2.fit.csv"))
+A3 <- as.matrix(read.table("../../results/Spain/spain.Treatment.full.alphas.3.fit.csv"))
+A4 <- as.matrix(read.table("../../results/Spain/spain.Treatment.full.alphas.4.fit.csv"))
+#A <- A1 + A2 + A3 #as.matrix(read.table("../../results/Godoy/godoy.T.alphas.csv"))
 
 rownames(A) <- colnames(A) <- letters[1:nrow(A)]
 rownames(A1) <- colnames(A1) <- letters[1:nrow(A1)]
 rownames(A2) <- colnames(A2) <- letters[1:nrow(A2)]
 rownames(A3) <- colnames(A3) <- letters[1:nrow(A3)]
+rownames(A4) <- colnames(A4) <- letters[1:nrow(A4)]
 
 # A1 <- matrix(kronecker(1:10,1:10),10) / 100
 
 # get a baseline matrix
-A0 <- matrix(mean(A),nrow(A1),ncol(A1))
+# A0 <- matrix(mean(A),nrow(A1),ncol(A1))
 
 # make the first dimension relative
-A1 <- A1 - A0
+# A1 <- A1 - A0
 
 weights <- c(0.9,0.9)
-hmm <- weights[1]*rowMeans(A1) + weights[2]*colMeans(A1)
+hmm <- weights[1]*rowMeans(A) + weights[2]*colMeans(A)
 # hmm <- 0.5*(apply(A,1,max) + apply(A,2,max))
 # # # hmm <- 0.5*(apply(A,))
 hmm.order <- (names(sort(rank(hmm),decreasing=TRUE))) #order(hmm, decreasing=TRUE)
@@ -37,17 +41,22 @@ A1 <- A1[,hmm.order]
 
 A2 <- A2[hmm.order,]
 A2 <- A2[,hmm.order]
+
 A3 <- A3[hmm.order,]
 A3 <- A3[,hmm.order]
+
+A4 <- A4[hmm.order,]
+A4 <- A4[,hmm.order]
+
 # A1 <- A1[hmm.order,rev(hmm.order)]
 # A2 <- A2[hmm.order,rev(hmm.order)]
 # A3 <- A3[hmm.order,rev(hmm.order)]
 
-fmax <- max((c(A1,A2,A3,A)))
-fmin <- min((c(A1,A2,A3,A)))
+fmax <- max((c(A,A1,A2,A3,A4)))
+fmin <- min((c(A,A1,A2,A3,A4)))
 
 setEPS(width=17, height=8)
-postscript('../../manuscript/Figures/alphas.godoy.T.eps')
+postscript('../../manuscript/Figures/alphas.spain.Treatment.eps')
 
 layout(mat = rbind(
 		c(5,6,1,2),
@@ -57,22 +66,22 @@ layout(mat = rbind(
        widths = c(2, 0.3, 1, 1)
 )
 
-par(mar = c(5, 6, 4.5, 0), oma = c(0, 0, 0, 0.75))
+par(mar = c(5, 6, 4.5, 1.0), oma = c(0, 0, 0, 0.75))
 
 # layout.show(6)
 
 # add a color scale for fitness
 
 pal <- c(
-	rev(colorRampPalette(brewer.pal(9, "Blues"))(100)),
-	(colorRampPalette(brewer.pal(9, "Reds"))(200))
+	rev(colorRampPalette(brewer.pal(9, "Blues"))(150)),
+	(colorRampPalette(brewer.pal(9, "Reds"))(550))
 )
 
 # plot the mean overall
 plot(
-	A0,
+	A1,
 	col=pal,
-	breaks=seq(-1.5, 3, length.out=length(pal)),
+	breaks=seq(-1.5, 5.5, length.out=length(pal)),
 	key=NULL,
 	xaxt='n',
 	yaxt='n',
@@ -85,16 +94,16 @@ plot(
 	asp=1
 )
 
-title('b  Mean interaction', line=1.2, cex.main=2.3)
+title('b First dimension (91.0%)', line=1.2, cex.main=2.3)
 mtext("Effect species", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.75)
 mtext("Response species", 2, outer=FALSE, line=0.0, xpd=NA, cex=1.75)
 
 
 # plot the first axis
 plot(
-	A1,
+	A2,
 	col=pal,
-	breaks=seq(-1.5, 3, length.out=length(pal)),
+	breaks=seq(-1.5, 5.5, length.out=length(pal)),
 	key=NULL,
 	xaxt='n',
 	yaxt='n',
@@ -107,15 +116,15 @@ plot(
 	asp=1
 )
 
-title('c  First dimension (15.7%)', line=1.2, cex.main=2.3)
+title('c Second dimension (3.7%)', line=1.2, cex.main=2.3)
 mtext("Effect species", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.75)
 mtext("Response species", 2, outer=FALSE, line=0.0, xpd=NA, cex=1.75)
 
 # par(mar = c(1, 1, 1, 1))
 plot(
-	A2,
+	A3,
 	col=pal,
-	breaks=seq(-1.5, 3, length.out=length(pal)),
+	breaks=seq(-1.5, 5.5, length.out=length(pal)),
 	key=NULL,
 	xaxt='n',
 	yaxt='n',
@@ -127,7 +136,7 @@ plot(
 	axis.row=NULL,
 	asp=1
 )
-title('d  Second dimension (5.6%)', line=1.2, cex.main=2.3, xpd=NA)
+title('d Third dimension (2.3%)', line=1.2, cex.main=2.3, xpd=NA)
 mtext("Effect species", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.75)
 mtext("Response species", 2, outer=FALSE, line=0.0, xpd=NA, cex=1.75)
 # mtext("+", 2, outer=FALSE, line=3.7, xpd=NA, cex=3.5)
@@ -141,9 +150,9 @@ mtext("Response species", 2, outer=FALSE, line=0.0, xpd=NA, cex=1.75)
 # # plot the difference
 # par(mar = c(4, 0, 5, 7))
 plot(
-	A3,
+	A4,
 	col=pal,
-	breaks=seq(-1.5, 3, length.out=length(pal)),
+	breaks=seq(-1.5, 5.5, length.out=length(pal)),
 	key=NULL,
 	xaxt='n',
 	yaxt='n',
@@ -155,7 +164,7 @@ plot(
 	axis.row=NULL,
 	asp=1
 )
-title('e  Third dimension (3.0%)', line=1.2, cex.main=2.3, xpd=NA)
+title('e Fourth dimension (2.0%)', line=1.2, cex.main=2.3, xpd=NA)
 mtext("Effect species", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.75)
 mtext("Response species", 2, outer=FALSE, line=0.0, xpd=NA, cex=1.75)
 # mtext("+", 2, outer=FALSE, line=3.7, xpd=NA, cex=3.5)
@@ -165,7 +174,7 @@ mtext("Response species", 2, outer=FALSE, line=0.0, xpd=NA, cex=1.75)
 plot(
 	A,
 	col=pal,
-	breaks=seq(-1.5, 3, length.out=length(pal)),
+	breaks=seq(-1.5, 5.5, length.out=length(pal)),
 	key=NULL,
 	xaxt='n',
 	yaxt='n',
