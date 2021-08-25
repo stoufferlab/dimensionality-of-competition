81cf5ad25537275cbee5becb9083d44508795cfe
Daniel B. Stouffer <dbstouffer@gmail.com>
2021-08-25 13:24:28 +1200

    only results for interaction matrices

--- code/Figures/variance_explained.R
+++ code/Figures/variance_explained.R
@@ -4,41 +4,52 @@ library(RColorBrewer)
 # library(plot.matrix)
 
 # read in GOF by dimensionality measures
-godoyC <- read.table("../../results/Godoy/godoy.C.pseudo-rsquared.csv")
-godoyT <- read.table("../../results/Godoy/godoy.T.pseudo-rsquared.csv")
+godoyC <- "../../results/Spain/spain.Control.pseudo-rsquared.csv"
+godoyT <- "../../results/Spain/spain.Treatment.pseudo-rsquared.csv"
 
-# these datasets were fit from Levine matrix only
-wainwright.dir <- "../../results/Wainwright/"
-wainwright.files <- list.files(wainwright.dir, "rsquared")
-wainwright.files <- paste0(wainwright.dir, wainwright.files)
+wainwrightOpen <- "../../results/Australia/australia.Open.pseudo-rsquared.csv"
+wainwrightShade <- "../../results/Australia/australia.Shade.pseudo-rsquared.csv"
 
-# these datasets were fit from Levine matrix only
-levine.dir <- "../../results/Levine/"
-levine.files <- list.files(levine.dir, "rsquared")
-levine.files <- paste0(levine.dir, levine.files)
+# # these datasets were fit from Levine matrix only
+# levine.dir <- "../../results/Levine/"
+# levine.files <- list.files(levine.dir, "rsquared")
+# levine.files <- paste0(levine.dir, levine.files)
 
 # these datasets were fit from Kinlock matrix only
 kinlock.dir <- "../../results/Kinlock/"
 kinlock.files <- list.files(kinlock.dir, "rsquared")
 kinlock.files <- paste0(kinlock.dir, kinlock.files)
 
-# remove Engel due to errors in the data
-kinlock.files <- grep("Engel",kinlock.files,invert=TRUE,value=TRUE)
-# remove Goldberg & Landa because we analyse it the other way
-kinlock.files <- grep("Landa",kinlock.files,invert=TRUE,value=TRUE)
+# remove Engel due to errors in the data and we have it from Levine
+# remove Goldberg & Landa because we have it from Levine
+# kinlock.files <- grep("Engel|Landa",kinlock.files,invert=TRUE,value=TRUE)
+
+# put all files together
+all.files <- c(
+	godoyC,
+	godoyT,
+	wainwrightOpen,
+	wainwrightShade,
+	kinlock.files
+)
+
+# read in the data sets
+rsquareds <- sapply(all.files, function(x) read.table(x), simplify=FALSE)
+
+rsquareds <- rsquareds[order(sapply(rsquareds, nrow), decreasing=TRUE)]
 
 # where to save the figure
 setEPS(width=15, height=15)
 postscript('../../manuscript/Supplementary/Figures/variance_explained.eps')
 
 layout(mat = matrix(
-		1:36,
+		1:16,
 		byrow=TRUE,
-        nrow = 6, 
-        ncol = 6
+        nrow = 4, 
+        ncol = 4
        ),
        heights = c(1),
-       widths = c(2, 2, 2, 2, 2, 2)
+       widths = c(2, 2, 2, 2)
 )
 
 par(mar = c(2, 2.5, 2, 2.5), oma = c(3.5, 4.5, 0.5, 0.5))
@@ -47,92 +58,7 @@ par(mar = c(2, 2.5, 2, 2.5), oma = c(3.5, 4.5, 0.5, 0.5))
 cex.axis <- 2
 padj <- 0.25
 
-# add a color scale for fitness
 
-# plot(
-# 	c(0,1:nrow(godoyC)),
-# 	c(0,godoyC[,2]),
-# 	xaxs='i',
-# 	yaxs='i',
-# 	axes=FALSE,
-# 	xlim=c(-0.5,0.5+nrow(godoyC)),
-# 	ylim=c(0,1.0),
-# 	type='l'
-# )
-# axis(
-# 	1,
-# 	at=seq(0,1+nrow(godoyC),2),
-# 	tcl=0.5,
-# 	cex.axis=cex.axis,
-# 	padj=padj,
-# 	gap.axis=0
-# )
-# axis(
-# 	2,
-# 	at=seq(0,1.0,0.2),
-# 	tcl=0.5,
-# 	cex.axis=cex.axis,
-# 	# padj=padj,
-# 	las=1
-# )
-# # abline(v=1,lwd=2.5,lty='dotted')
-# abline(v=3,lwd=2.5,lty='dashed')
-# points(
-# 	c(0,1:nrow(godoyC)),
-# 	c(0,godoyC[,2]),
-# 	pch=21,
-# 	lwd=1.5,
-# 	bg="#a6cee3",
-# 	cex=2,
-# 	xpd=TRUE
-# )
-# text("Dataset 1", x=7, y=0.8, xpd=NA, cex=2, font=2, pos=3)
-
-# plot(
-# 	c(0,1:nrow(godoyT)),
-# 	c(0,godoyT[,2]),
-# 	xaxs='i',
-# 	yaxs='i',
-# 	axes=FALSE,
-# 	xlim=c(-0.5,0.5+nrow(godoyT)),
-# 	ylim=c(0, 1.0),
-# 	type='l'
-# )
-# axis(
-# 	1,
-# 	at=seq(0,1+nrow(godoyT),2),
-# 	tcl=0.5,
-# 	cex.axis=cex.axis,
-# 	padj=padj,
-# 	gap.axis=0
-# )
-# axis(
-# 	2,
-# 	at=seq(0,1,0.2),
-# 	tcl=0.5,
-# 	cex.axis=cex.axis,
-# 	# padj=padj,
-# 	las=1
-# )
-# # abline(v=1,lwd=2.5,lty='dotted')
-# abline(v=3,lwd=2.5,lty='dashed')
-# points(
-# 	c(0,1:nrow(godoyT)),
-# 	c(0,godoyT[,2]),
-# 	pch=21,
-# 	lwd=1.5,
-# 	bg="#a6cee3",
-# 	cex=2,
-# 	xpd=TRUE
-# )
-# text("Dataset 2", x=7, y=0.8, xpd=NA, cex=2, font=2, pos=3)
-
-# other data sets
-rsquareds <- c(levine.files, kinlock.files)
-
-rsquareds <- sapply(rsquareds, function(x) read.table(x), simplify=FALSE)
-
-rsquareds <- rsquareds[order(sapply(rsquareds, nrow), decreasing=TRUE)]
 
 # wainwrights <- sapply(wainwright.files, function(x) read.table(x), simplify=FALSE)
 
@@ -171,7 +97,7 @@ for(i in 1:(length(rsquareds))){
 	)
 
 	abline(
-		v = igraph::dim_select(d[,1]),
+		v = min(which(d[,3]>0.95)),
 		lwd=2.5,
 		lty="dashed"
 	)
@@ -186,10 +112,18 @@ for(i in 1:(length(rsquareds))){
 		xpd=TRUE
 	)
 
-	text(paste0("Dataset ",i+2), x=0.7*nrow(d), y=0.8, xpd=NA, cex=2, font=2, pos=3)
+	l <- switch(as.character(i),
+		"1" = "1-Wet",
+		"2" = "1-Dry",
+		"3" = "2-Sun",
+		"4" = "2-Shade",
+		i - 2
+	)
+		
+	text(paste0("Dataset ",l), x=0.7*nrow(d), y=0.8, xpd=NA, cex=2, font=2, pos=3)
 
 	if(i == 2){
-		mtext("Variance explained per dimension", 2, outer=TRUE, line=1.8, cex=2)
+		mtext("Niche variation explained per niche dimension", 2, outer=TRUE, line=1.8, cex=2)
 	}
 
 	if(i == 6){
