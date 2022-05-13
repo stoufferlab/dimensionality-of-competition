50e8a1b64a6d179d18d637eb8fac277abf68b3a3
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2022-05-13 14:36:52 +1200

    code to produce various plots comparing empirical data to their null expectations in terms of dimensionality

---
+++ code/Null/empirical-null-1.R
@@ -0,0 +1,256 @@
+
+# read in the Kinlock data
+kinlock <- read.csv("../../data/Kinlock/Data_PlantInteractionNetworks.csv")
+
+# remove small datasets
+kinlock <- subset(kinlock, SpeciesRichness >= 3)
+
+# convert Kinlock's RII to alphas or relative yield
+kinlock.transformed <- apply(
+	kinlock,
+	1,
+	function(x){
+		res <- as.list(x)
+		spp <- as.integer(x["SpeciesRichness"])
+		if(x["TypeOfControl"] == "True ctrl"){
+			res$RII <- matrix(
+				as.numeric(strsplit(as.character(x["NetworkTrueCtrl"]),",")[[1]]),
+				byrow=TRUE,
+				nrow=spp,
+				ncol=spp
+			)
+			res$alphaijNj <- (1 - res$RII)/(1 + res$RII) - 1
+			res$usable <- ifelse(any(is.na(res$alphaijNj)),FALSE,TRUE)
+		}else{
+			res$RII <- matrix(
+				as.numeric(strsplit(as.character(x["NetworkMonoCtrl"]),",")[[1]]),
+				byrow=TRUE,
+				nrow=spp,
+				ncol=spp
+			)
+			res$RelativeYield <- (1 + res$RII)/(1 - res$RII)
+			res$usable <- FALSE #ifelse(any(is.na(res$RelativeYield)),FALSE,TRUE)
+		}
+		
+		return(res)
+	}
+)
+
+# keep only "full" matrices
+kinlock.full <- kinlock.transformed[which(sapply(kinlock.transformed,function(x) x$usable))]
+
+# get rid of unnecessary data
+kinlock.full <- lapply(
+	kinlock.full,
+	function(x) {list(observed = x$alphaijNj)}
+)
+
+# read in the full dimension fits of the australian data sets
+australia.full <- list()
+australia.full[["Open"]] <- list(observed=read.table('../../results/Australia/australia.Open.full.alphas.fit.csv'))
+australia.full[["Shade"]] <- list(observed=read.table('../../results/Australia/australia.Shade.full.alphas.fit.csv'))
+
+# read in the full dimension fits of the spanish data sets
+spain.full <- list()
+spain.full[["Control"]] <- list(observed=read.table('../../results/Spain/spain.Control.full.alphas.fit.csv'))
+spain.full[["Treatment"]] <- list(observed=read.table('../../results/Spain/spain.Treatment.full.alphas.fit.csv'))
+
+# combine all observed datasets together
+observed.data <- c(kinlock.full, australia.full, spain.full)
+
+dhat <- function(amat, threshold=0.95){
+	SVD <- svd(amat)
+	rsq <- (SVD$d**2)/sum((SVD$d**2))
+	rsq.cum <- cumsum(rsq)
+	dhat <- min(which(rsq.cum > threshold))
+	# dhat <- igraph::dim_select(SVD$d)
+	# dhat <- weighted.mean(1:length(rsq), rsq)
+	return(dhat)
+}
+
+# perform an svd as appropriate
+observed.data <- lapply(
+	observed.data,
+	function(x) {
+		x$dhat <- dhat(x$observed)
+		return(x)
+	}
+)
+
+# number of randomizations to use
+nrand <- 250
+
+# generate a null distribution of dimensionality values for the kinlock data
+null.data <- lapply(
+	observed.data,
+	function(x, nrand){
+		x$dhat.null <- sapply(
+			1:nrand,
+			function(x,observed){
+				rand.mat <- matrix(sample(unlist(observed)),nrow(observed),ncol(observed))
+				return(dhat(rand.mat))
+			},
+			observed = x$observed
+		)
+	},
+	nrand = nrand
+)
+
+# generate some quantities for the plot
+spp <- sapply(observed.data, function(x) nrow(x$observed))
+obs <- sapply(observed.data, function(x) x$dhat)
+null.mean <- sapply(null.data, mean)
+null.sd <- sapply(null.data, sd)
+null.se <- null.sd / nrand
+
+# make a pretty picture
+setEPS(width=4.0, height=3.7)
+postscript('../../manuscript/Supplementary/Figures/dimensionality_with_null.eps')
+
+par(mar = c(2, 2.5, 0.5, 1.0), oma = c(2.5, 2.5, 0.75, 1.0))
+
+cex.axis <- 1.7
+padj <- 0
+
+plot(
+	spp-0.125,
+	obs,
+	xlim=c(1,12),
+	ylim=c(1,12),
+	# xlab="Species richness",
+	# ylab="Dimensionality",
+	pch=21,
+	bg="#a6cee3",
+	xaxs='i',
+	yaxs='i',
+	type='n',
+	axes=FALSE,
+	xlab='',
+	ylab=''
+)
+
+# add the x axis
+axis(
+	1,
+	at=c(1,4,8,12),
+	tcl=0.5,
+	cex.axis=cex.axis,
+	padj=padj,
+	gap.axis=0
+)
+
+# add the y axis
+axis(
+	2,
+	at=c(1,4,8,12),
+	# labels=c("","1","4","8","12"),
+	tcl=0.5,
+	cex.axis=cex.axis,
+	# padj=padj,
+	las=1
+)
+
+abline(0,1,lty='dotted')
+
+# show what the prediction for the random data is
+xpred <- seq(1,12,length.out=1000)
+
+# prediction based on randomized interactions
+prediction <- predict(
+	glm(
+		null.mean ~ 0+I(spp-1),
+		family="gaussian",
+		offset=rep(1,length(spp))
+		# weights=1/null.sd
+	),
+	newdata=data.frame(spp=xpred),
+	#type="response",
+	se.fit=TRUE
+)
+
+polygon(
+	x=c(xpred, rev(xpred)),
+	y=(c(prediction$fit+prediction$se.fit, rev(prediction$fit-prediction$se.fit))),
+	col=colors()[404],
+	border=NA
+)
+
+lines(
+	x=xpred,
+	y=(prediction$fit),
+	col=grey(0.25)
+)
+
+# prediction for observed data
+prediction <- predict(
+	glm(obs ~ 0+I(spp-1), family="gaussian", offset=rep(1,length(spp))),
+	newdata=data.frame(spp=xpred),
+	#type="response",
+	se.fit=TRUE
+)
+
+polygon(
+	x=c(xpred, rev(xpred)),
+	y=(c(prediction$fit+prediction$se.fit, rev(prediction$fit-prediction$se.fit))),
+	col=grey(0.75),
+	border=NA
+)
+
+lines(
+	x=xpred,
+	y=(prediction$fit),
+	col=grey(0.25)
+)
+
+
+segments(
+	x0=spp-0.125,
+	x1=spp+0.125,
+	y0=obs,
+	y1=null.mean,
+	lwd=1.5,
+	lty=2,
+	col=grey(0.33)
+)
+
+segments(
+	x0=spp-0.125,
+	x1=spp+0.125,
+	y0=obs,
+	y1=null.mean,
+	lwd=1.5,
+	lty=2,
+	col=grey(0.5)
+)
+
+segments(
+	x0=spp+0.125,
+	x1=spp+0.125,
+	y0=null.mean + null.sd,
+	y1=null.mean - null.sd
+)
+
+points(
+	spp-0.125,
+	obs,
+	pch=21,
+	bg="#a6cee3",
+	cex=1,
+	xpd=TRUE
+	# col="#a6cee3"
+)
+
+points(
+	spp+0.125,
+	null.mean,
+	# col='red',
+	pch=22,
+	bg='red',
+	cex=1
+)
+
+mtext("Niche dimensionality", 2, line=3.15, xpd=NA, adj=0.5, cex=2)
+
+mtext("Species richness", 1, outer=FALSE, xpd=NA, line=3.15, cex=2, adj=0.50)
+
+dev.off()
---
+++ code/Null/empirical-null-2.R
@@ -0,0 +1,232 @@
+
+library(RColorBrewer)
+
+# read in the Kinlock data
+kinlock <- read.csv("../../data/Kinlock/Data_PlantInteractionNetworks.csv")
+
+# remove small datasets
+kinlock <- subset(kinlock, SpeciesRichness >= 3)
+
+# convert Kinlock's RII to alphas or relative yield
+kinlock.transformed <- apply(
+	kinlock,
+	1,
+	function(x){
+		res <- as.list(x)
+		spp <- as.integer(x["SpeciesRichness"])
+		if(x["TypeOfControl"] == "True ctrl"){
+			res$RII <- matrix(
+				as.numeric(strsplit(as.character(x["NetworkTrueCtrl"]),",")[[1]]),
+				byrow=TRUE,
+				nrow=spp,
+				ncol=spp
+			)
+			res$alphaijNj <- (1 - res$RII)/(1 + res$RII) - 1
+			res$usable <- ifelse(any(is.na(res$alphaijNj)),FALSE,TRUE)
+		}else{
+			res$RII <- matrix(
+				as.numeric(strsplit(as.character(x["NetworkMonoCtrl"]),",")[[1]]),
+				byrow=TRUE,
+				nrow=spp,
+				ncol=spp
+			)
+			res$RelativeYield <- (1 + res$RII)/(1 - res$RII)
+			res$usable <- FALSE #ifelse(any(is.na(res$RelativeYield)),FALSE,TRUE)
+		}
+		
+		return(res)
+	}
+)
+
+# keep only "full" matrices
+kinlock.full <- kinlock.transformed[which(sapply(kinlock.transformed,function(x) x$usable))]
+
+# get rid of unnecessary data
+kinlock.full <- lapply(
+	kinlock.full,
+	function(x) {list(observed = x$alphaijNj)}
+)
+
+# read in the full dimension fits of the australian data sets
+australia.full <- list()
+australia.full[["Open"]] <- list(observed=read.table('../../results/Australia/australia.Open.full.alphas.fit.csv'))
+australia.full[["Shade"]] <- list(observed=read.table('../../results/Australia/australia.Shade.full.alphas.fit.csv'))
+
+# read in the full dimension fits of the spanish data sets
+spain.full <- list()
+spain.full[["Control"]] <- list(observed=read.table('../../results/Spain/spain.Control.full.alphas.fit.csv'))
+spain.full[["Treatment"]] <- list(observed=read.table('../../results/Spain/spain.Treatment.full.alphas.fit.csv'))
+
+# combine all observed datasets together
+observed.data <- c(spain.full, australia.full, kinlock.full)
+
+# put the data in a consistent order
+observed.data <- observed.data[order(sapply(observed.data, function(x) nrow(x$observed)), decreasing=TRUE)]
+
+# function to do the svd magic
+dhat <- function(amat, threshold=0.95){
+	SVD <- svd(amat)
+	rsq <- (SVD$d**2)/sum((SVD$d**2))
+	rsq.cum <- cumsum(rsq)
+	dhat <- min(which(rsq.cum > threshold))
+	# dhat <- igraph::dim_select(SVD$d)
+	# dhat <- weighted.mean(1:length(rsq), rsq)
+	return(list(rsq=rsq, rsq.cum=rsq.cum, dhat=dhat))
+}
+
+# perform an svd as appropriate
+observed.data <- lapply(
+	observed.data,
+	function(x) {
+		return(c(x,dhat(x$observed)))
+	}
+)
+
+# number of randomizations to use
+nrand <- 250
+
+# generate a null distribution of dimensionality values for the kinlock data
+null.data <- lapply(
+	observed.data,
+	function(x, nrand){
+		do.call(rbind,sapply(
+			1:nrand,
+			function(x,observed){
+				rand.mat <- matrix(sample(unlist(observed)),nrow(observed),ncol(observed))
+				return(dhat(rand.mat)$rsq.cum)
+			},
+			observed = x$observed,
+			simplify=FALSE
+		))
+	},
+	nrand = nrand
+)
+
+# where to save the figure
+setEPS(width=15, height=15)
+postscript('../../manuscript/Supplementary/Figures/rsquared_with_null.eps')
+
+layout(mat = matrix(
+		1:16,
+		byrow=TRUE,
+        nrow = 4, 
+        ncol = 4
+       ),
+       heights = c(1),
+       widths = c(2, 2, 2, 2)
+)
+
+par(mar = c(2, 2.5, 2, 2.5), oma = c(3.5, 4.5, 0.5, 0.5))
+
+# define some sizes
+cex.axis <- 2
+padj <- 0.25
+
+# let the magic happen
+set.seed(123)
+pal <- sample(colorRampPalette(brewer.pal(8, "Dark2"))(length(observed.data)))
+
+for(i in 1:(length(observed.data))){
+	obs <- observed.data[[i]]
+	nul <- null.data[[i]]
+	spp <- nrow(obs$observed)
+
+	plot(
+		NA,
+		NA,
+		xaxs='i',
+		yaxs='i',
+		axes=FALSE,
+		xlim=c(-0.5,0.5+spp),
+		ylim=c(0,1)
+	)
+
+	lines(
+		c(0,1:spp),
+		c(0,obs$rsq.cum)
+	)
+
+	axis(
+		1,
+		at=-1:(spp+2),
+		tcl=0.5,
+		cex.axis=cex.axis,
+		padj=padj
+	)
+	axis(
+		2,
+		at=seq(0,1,0.2),
+		tcl=0.5,
+		cex.axis=cex.axis,
+		# padj=padj,
+		las=1
+	)
+
+	points(
+		seq(0,ncol(nul),1),
+		c(0,colMeans(nul)),
+		pch=22,
+		bg='grey',
+		type='b',
+		cex=1.5,
+		col=grey(0.5)
+	)
+
+	segments(
+		x0=seq(0,ncol(nul),1),
+		x1=seq(0,ncol(nul),1),
+		y0=c(0,colMeans(nul))+c(0,apply(nul,2,sd)),
+		y1=c(0,colMeans(nul))-c(0,apply(nul,2,sd)),
+		col=grey(0.5)
+	)
+
+
+
+	# abline(
+	# 	v = obs$dhat,
+	# 	lwd=2.5,
+	# 	lty="dashed"
+	# )
+
+	points(
+		c(0,1:spp),
+		c(0,obs$rsq.cum),
+		pch=21,
+		lwd=1.5,
+		bg=pal[i],
+		cex=2,
+		xpd=TRUE
+	)
+
+	# for(j in 1:nrow(nul)){
+	# 	points(
+	# 		seq(0,ncol(nul),1),
+	# 		c(0,nul[j,]),
+	# 		pch=22,
+	# 		bg='grey',
+	# 		type='l',
+	# 		col=grey(0.5)
+	# 	)
+	# }
+
+	l <- switch(as.character(i),
+		"1" = "1-Wet",
+		"2" = "1-Dry",
+		"3" = "2-Sun",
+		"4" = "2-Shade",
+		i - 2
+	)
+		
+	text(paste0("Dataset ",l), x=0.7*spp, y=0.8, xpd=NA, cex=2, font=2, pos=3)
+
+	if(i == 2){
+		mtext("Total niche variation explained", 2, outer=TRUE, line=1.8, cex=2)
+	}
+
+	if(i == 6){
+		mtext("Total niche dimensions", 1, outer=TRUE, line=1.8, cex=2)
+	}
+
+}
+
+dev.off()
