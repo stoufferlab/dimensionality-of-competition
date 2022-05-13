1b7a67c157b15e03d065f396c983617faa5d5e32
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2022-05-13 14:39:38 +1200

    change raw data to state dimensionality of full fits; fix error which led to black boxes in australia plots

--- code/Figures/alpha_matrices_obs_fit.R
+++ code/Figures/alpha_matrices_obs_fit.R
@@ -19,10 +19,10 @@ alpha.fits <- c(
 )
 
 alpha.orig <- c(
-	"../../results/Spain/spain.Control.alphas.regression.csv",
-	"../../results/Spain/spain.Treatment.alphas.regression.csv",
-	"../../results/Australia/australia.Open.alphas.regression.csv",
-	"../../results/Australia/australia.Shade.alphas.regression.csv",
+	"../../results/Spain/spain.Control.full.alphas.fit.csv",
+	"../../results/Spain/spain.Treatment.full.alphas.fit.csv",
+	"../../results/Australia/australia.Open.full.alphas.fit.csv",
+	"../../results/Australia/australia.Shade.full.alphas.fit.csv",
 	gsub("fit", "orig", kinlock.alphas)
 )
 
@@ -75,6 +75,11 @@ for(j in (1+4*(i-1)):min(4*i,length(stems))){
 	orig <- read.table(alpha.orig[j])
 	fit <- read.table(alpha.fits[j])
 
+	if(j %in% c(3,4)){
+		orig$Other <- NULL
+		fit$Other <- NULL
+	}
+
 	orig <- as.matrix(orig)
 	fit <- as.matrix(fit)
 
@@ -105,7 +110,11 @@ for(j in (1+4*(i-1)):min(4*i,length(stems))){
 		na.col='black'
 	)
 
-	title('Observed', line=1.2, cex.main=2.3)
+	if(i==1){
+		title(paste0(nrow(orig),'-dimension fit'), line=1.2, cex.main=2.3, xpd=TRUE)
+	}else{
+		title('Observed', line=1.2, cex.main=2.3)
+	}
 	mtext("Effect species", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.75)
 	mtext("Response species", 2, outer=FALSE, line=1., xpd=NA, cex=1.75)
 
@@ -179,130 +188,3 @@ for(j in (1+4*(i-1)):min(4*i,length(stems))){
 dev.off()
 
 }
-
-# XX
-
-# # add a color scale for fitness
-
-# pal <- c(
-# 	rev(colorRampPalette(brewer.pal(9, "Blues"))(88)),
-# 	(colorRampPalette(brewer.pal(9, "Reds"))(212))
-# )
-
-# # plot the first axis
-# plot(
-# 	A1,
-# 	col=pal,
-# 	breaks=seq(-5, 12, length.out=length(pal)),
-# 	key=NULL,
-# 	xaxt='n',
-# 	yaxt='n',
-# 	xlab='',
-# 	ylab='',
-# 	main='',
-# 	axes=FALSE,
-# 	axis.col=NULL,
-# 	axis.row=NULL
-# )
-
-# title('(B) First resource (32.6%)', line=1.2, cex.main=2.3)
-# mtext("Effect species", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.75)
-# mtext("Response species", 2, outer=FALSE, line=1., xpd=NA, cex=1.75)
-
-# # par(mar = c(1, 1, 1, 1))
-# plot(
-# 	A2,
-# 	col=pal,
-# 	breaks=seq(-5, 12, length.out=length(pal)),
-# 	key=NULL,
-# 	xaxt='n',
-# 	yaxt='n',
-# 	xlab='',
-# 	ylab='',
-# 	main='',
-# 	axes=FALSE,
-# 	axis.col=NULL,
-# 	axis.row=NULL
-# )
-# title('(C) Second resource (21.7%)', line=1.2, cex.main=2.3, xpd=NA)
-# mtext("Effect species", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.75)
-# mtext("Response species", 2, outer=FALSE, line=1., xpd=NA, cex=1.75)
-# mtext("+", 2, outer=FALSE, line=3.7, xpd=NA, cex=3.5)
-
-#  #0,max(max(no.comp),max(with.comp)))) # xlab = expression('s'['m']*' = 1 - w'['jm']), ylab=expression('s'['f']*' = 1 - w'['kf']))
-# # image(with.comp, xlab = 'z', ylab='Env', main='With competition', col=pal, zlim=c(fmin, fmax)) #, zlim=c(0,max(max(no.comp),max(with.comp)))) # xlab = expression('s'['m']*' = 1 - w'['jm']), ylab=expression('s'['f']*' = 1 - w'['kf']))
-
-# # pal <- brewer.pal(9, "Blues")
-# #pal <- brewer.pal(9, "Greys")
-
-# # # plot the difference
-# # par(mar = c(4, 0, 5, 7))
-# plot(
-# 	A3,
-# 	col=pal,
-# 	breaks=seq(-5, 12, length.out=length(pal)),
-# 	key=NULL,
-# 	xaxt='n',
-# 	yaxt='n',
-# 	xlab='',
-# 	ylab='',
-# 	main='',
-# 	axes=FALSE,
-# 	axis.col=NULL,
-# 	axis.row=NULL
-# )
-# title('(D) Third resource (16.4%)', line=1.2, cex.main=2.3, xpd=NA)
-# mtext("Effect species", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.75)
-# mtext("Response species", 2, outer=FALSE, line=1., xpd=NA, cex=1.75)
-# mtext("+", 2, outer=FALSE, line=3.7, xpd=NA, cex=3.5)
-# # mtext("Environment", 2, outer=FALSE, line=1., xpd=NA, cex=1.5)
-# # mtext("Trait", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.5)
-
-# # # plot the difference
-# # par(mar = c(4, 0, 5, 7))
-# plot(
-# 	A,
-# 	col=pal,
-# 	breaks=seq(-5, 12, length.out=length(pal)),
-# 	key=NULL,
-# 	xaxt='n',
-# 	yaxt='n',
-# 	xlab='',
-# 	ylab='',
-# 	main='',
-# 	axes=FALSE,
-# 	axis.col=NULL,
-# 	axis.row=NULL
-# )
-# title(main=list(c('a',' Interaction matrix')), line=1.2, cex.main=2.3)
-# mtext("Effect species", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.75)
-# mtext("Response species", 2, outer=FALSE, line=1., xpd=NA, cex=1.75)
-# text(14.6, 5.5, "=", outer=TRUE, line=1, xpd=NA, cex=4.5)
-# # mtext("Environment", 2, outer=FALSE, line=1., xpd=NA, cex=1.5)
-# # mtext("Trait", 1, outer=FALSE, line=1.5, xpd=NA, cex=1.5)
-
-
-# # add a color scale for 
-
-# # pal <- brewer.pal(9, "Blues")
-# # pal <- colorRampPalette(pal)(10000)
-# par(mar = c(5, 1.5, 4.5, 3.0))
-# colorbarr <- t(matrix(seq(-5, 12, length.out=length(pal)), length(pal), 1))
-# image(
-# 	colorbarr,
-# 	col=pal,
-# 	xaxt='n',
-# 	yaxt='n',
-# 	mgp=c(0.5,0.5,0.5)
-# )
-# text(1.75, 0.5, "Interaction strength", outer=FALSE, line=1., xpd=NA, cex=2.25, srt=270)
-# mtext("+", 1, outer=FALSE, line=1., xpd=NA, cex=1.75)
-# mtext("-", 3, outer=FALSE, line=0.5, xpd=NA, cex=1.75)
-
-# # dev.copy(device=x11)
-# # dev.print()#height=3, width=7)
-
-# dev.off()
-
-# # # comp <- expand.grid(z,E)
-# # # comp$N <- N[2]
